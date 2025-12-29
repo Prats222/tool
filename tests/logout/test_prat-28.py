5bad0ea17202199741ddc355a834bfae1234f2d4
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2025-12-29 23:04:16 +0530

    Add Selenium test for PRAT-28

---
+++ tests/logout/test_prat-28.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_prat-28():
+    """Jira: PRAT-28"""
+    # Preconditions:
+    # User is logged in
+    # Steps:
+    # Click on profile menu \n    #  Click Logout
+    # Expected:
+    # User should be logged out and redirected to login page
+    assert True
