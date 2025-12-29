3ead0a67acb9c4153aee00e1cb0f0b6b2399e65b
Prateek Mishra <126672828+Prats222@users.noreply.github.com>
2025-12-29 23:01:17 +0530

    Add Selenium test for PRAT-24

---
+++ tests/login/test_prat-24.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_prat-24():
+    """Jira: PRAT-24"""
+    # Preconditions:
+    # User is registered and on login page
+    # Steps:
+    # Enter valid username \n    #  Enter invalid password \n    #  Click Login
+    # Expected:
+    # Error message should be displayed indicating invalid credentials
+    assert True
